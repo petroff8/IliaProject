557dbc6366d46aa372949fbc7ef0a72640345ebf
petroff8 <60380844+petroff8@users.noreply.github.com>
2020-02-10 12:37:27 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Page Title</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+}
+
+/* Style the header */
+.header {
+  padding: 80px;
+  text-align: center;
+  background: #1abc9c;
+  color: white;
+}
+
+/* Increase the font size of the h1 element */
+.header h1 {
+  font-size: 40px;
+}
+</style>
+</head>
+<body>
+
+<div class="header">
+  <h1>NOVINI</h1>
+  <p>Think after read.</p>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/iZk86i5N8PE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+
+
+</body>
+</html>
+
+
